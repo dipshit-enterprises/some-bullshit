7a82666db65af95f528bfca6326e0372952ab456
dipshit-enterprises <160212554+dipshit-enterprises@users.noreply.github.com>
2024-02-16 00:02:49 -0800

    fucking module conflict with some random LG phone

--- dsep_bootanimation/Android.mk
+++ dsep_bootanimation/Android.mk
@@ -1,7 +1,8 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := bootanimation.zip
+LOCAL_MODULE := dsep_bootanimation
+LOCAL_SRC_FILES := bootanimation.zip
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT)/media
 LOCAL_SRC_FILES := $(LOCAL_MODULE)
