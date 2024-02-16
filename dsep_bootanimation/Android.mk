ec3fd86f1857fd66e15aa693c5a9dd4e5245df31
dipshit-enterprises <160212554+dipshit-enterprises@users.noreply.github.com>
2024-02-16 11:31:58 -0800

    I will apply the boot animation with a userscript instead

--- dsep_bootanimation/Android.mk
+++
@@ -1,9 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := dsep_bootanimation
-LOCAL_SRC_FILES := bootanimation.zip
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT)/media
-LOCAL_SRC_FILES := $(LOCAL_MODULE)
-include $(BUILD_PREBUILT)
