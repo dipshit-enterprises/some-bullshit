2d9ba4788dbbbdc53706e5e5038f010cca9be9b2
dipshit-enterprises <160212554+dipshit-enterprises@users.noreply.github.com>
2024-02-18 16:51:18 -0800

    actually define overlay correctly

--- products/gms.mk
+++ products/gms.mk
@@ -1,3 +1,4 @@
+LOCAL_PATH := $(call my-dir)
 PRODUCT_PACKAGES += \
 	GmsCore \
 	GsfProxy \
@@ -8,5 +9,5 @@ PRODUCT_PACKAGES += \
 	AuroraServices \
 	AuroraStore \
 	additional_repos.xml 
-PRODUCT_PACKAGE_OVERLAYS := overlay
+PRODUCT_PACKAGE_OVERLAYS := $(LOCAL_PATH)/overlay
 
